8e11e4e602c185563fc70e791d1c4ef194103376
Andy Hudson <andyhudson1979@gmail.com>
2022-09-11 20:40:05 +0100

    Timer function to remove completed tasks

---
+++ Randolph.ToDoFunctionApp.Tests/DeleteCompletedTasksTimerFunctionTests.cs
@@ -0,0 +1,67 @@
+﻿using System.Linq.Expressions;
+using Azure;
+using Azure.Data.Tables;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Timers;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Randolph.ToDoFunctionApp.Entities;
+
+namespace Randolph.ToDoFunctionApp.Tests;
+
+public class DeleteCompletedTasksTimerFunctionTests
+{
+    private readonly Mock<TimerInfo> _timerInfoMock;
+
+    private readonly Mock<TableClient> _tableClientMock;
+
+    private readonly Mock<ILogger> _loggerMock;
+
+    public DeleteCompletedTasksTimerFunctionTests()
+    {
+        var timerScheduleMock = new Mock<TimerSchedule>();
+        var scheduleStatusMock = new Mock<ScheduleStatus>();
+        this._timerInfoMock = new Mock<TimerInfo>(timerScheduleMock.Object, scheduleStatusMock.Object, false);
+        
+        this._tableClientMock = new Mock<TableClient>();
+        this._loggerMock = new Mock<ILogger>();
+    }
+    
+    [Fact]
+    public async Task ShouldDeleteCompletedTasksWhenRun()
+    {
+        // Arrange
+        var todos = new List<TodoTableEntity>
+        {
+            CreateEntity("Task 1", true),
+            CreateEntity("Task 2", true),
+            CreateEntity("Task 3", true)
+        };
+
+        var todoPages = Page<TodoTableEntity>.FromValues(todos.AsReadOnly(), null, Mock.Of<Response>());
+        var todoPagable = AsyncPageable<TodoTableEntity>.FromPages(new List<Page<TodoTableEntity>> { todoPages });
+
+        this._tableClientMock.Setup(x => x.QueryAsync(It.IsAny<Expression<Func<TodoTableEntity, bool>>>(),
+                It.IsAny<int?>(), It.IsAny<IEnumerable<string>>(), It.IsAny<CancellationToken>())).Returns(todoPagable);
+        
+        this._tableClientMock.Setup(x => x.DeleteEntityAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<ETag>(), It.IsAny<CancellationToken>())).Verifiable();
+
+        // Act
+        await DeleteCompletedTasksTimerFunction.RunAsync(this._timerInfoMock.Object, this._tableClientMock.Object, this._loggerMock.Object);
+
+        // Assert
+        this._tableClientMock.Verify(x => x.DeleteEntityAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<ETag>(), It.IsAny<CancellationToken>()), Times.Exactly(3));
+    }
+
+    private static TodoTableEntity CreateEntity(string taskDescription, bool isCompleted)
+    {
+        return new TodoTableEntity
+        {
+            RowKey = Guid.NewGuid().ToString("n"),
+            CreatedDt = DateTime.UtcNow,
+            PartitionKey = Constants.PartitionKey,
+            TaskDescription = taskDescription,
+            IsCompleted = isCompleted
+        };
+    }
+}
---
+++ Randolph.ToDoFunctionApp/DeleteCompletedTasksTimerFunction.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Azure.Data.Tables;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using Randolph.ToDoFunctionApp.Entities;
+using Randolph.ToDoFunctionApp.Extensions;
+using Randolph.ToDoFunctionApp.Models;
+
+namespace Randolph.ToDoFunctionApp;
+
+public static class DeleteCompletedTasksTimerFunction
+{
+    [FunctionName("DeleteCompletedTasksTimerFunction")]
+    public static async Task RunAsync(
+        [TimerTrigger("0 */1 * * * *")] TimerInfo myTimer,
+        [Table(Constants.ToDoTableName)] TableClient tableClient,
+        ILogger log)
+    {
+        log.LogInformation("C# Timer trigger function executed at: {UtcNow}", DateTime.UtcNow);
+
+        var completedTasks = tableClient.QueryAsync<TodoTableEntity>(todo => todo.IsCompleted == true);
+
+        await foreach (var currentPage in completedTasks.AsPages())
+        {
+            foreach (var currentEntity in currentPage.Values)
+            {
+                log.LogInformation("Deleting task {RowKey}", currentEntity.RowKey);
+                await tableClient.DeleteEntityAsync(Constants.PartitionKey, currentEntity.RowKey);
+            }
+        }
+    }
+}
